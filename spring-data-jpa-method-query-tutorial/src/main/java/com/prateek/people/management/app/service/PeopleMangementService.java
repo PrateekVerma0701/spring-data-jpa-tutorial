b6e745dfa5ca0049738ba29cf5605e0aa3545ac2
Prateek <prateek.verma0701@gmail.com>
2020-04-05 03:55:26 +0530

    added project to create method query

---
+++ spring-data-jpa-method-query-tutorial/src/main/java/com/prateek/people/management/app/service/PeopleMangementService.java
@@ -0,0 +1,13 @@
+package com.prateek.people.management.app.service;
+
+import java.util.List;
+
+import com.prateek.people.management.app.entities.Person;
+
+public interface PeopleMangementService {
+
+	List<Person> getPersonInfoByLastName(String lastName);
+
+	List<Person> getPersonInfoByFirstNameAndEmail(String firstName, String email);
+
+}
