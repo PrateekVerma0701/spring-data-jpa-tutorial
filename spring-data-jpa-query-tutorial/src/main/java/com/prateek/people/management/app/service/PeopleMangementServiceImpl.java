68920f6309c72d55eafe58886877fb48a04335eb
Prateek <prateek.verma0701@gmail.com>
2020-04-05 05:47:28 +0530

    Query project setup

---
+++ spring-data-jpa-query-tutorial/src/main/java/com/prateek/people/management/app/service/PeopleMangementServiceImpl.java
@@ -0,0 +1,28 @@
+package com.prateek.people.management.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.prateek.people.management.app.entities.Person;
+import com.prateek.people.management.app.repository.PeopleManagementRepositry;
+
+@Service
+public class PeopleMangementServiceImpl implements PeopleMangementService {
+
+	@Autowired
+	PeopleManagementRepositry peopleManagementRepositry;
+
+	@Override
+	public List<Person> getPersonInfoByLastName(String lastName) {
+		return peopleManagementRepositry.getPersonInfoByLastName(lastName);
+	}
+
+	@Override
+	public List<Person> getPersonInfoByFirstNameAndEmail(String firstName, String email) {
+		return peopleManagementRepositry.getPersonInfoByFirstNameAndEmail(firstName, email);
+	}
+
+}
